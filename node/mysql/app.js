6a39c2660232a13f0f3bf7b258a8a89eb9506b15
GSIII <kalseo901@gmail.com>
2025-03-28 15:05:03 +0900

    node mysql버전

--- node/mysql/app.js
+++ node/mysql/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
-const { appendFile } = require('fs');
 var mysql = require('mysql');
 const env = require('dotenv').config({ path: "../../.env" });
+var app = express();
 
 var connection = mysql.createConnection({
     host: process.env.host,
@@ -11,25 +11,25 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function (err) {
-    if (err) {
-        console.error('Database is connected~!!\n\n');
+    if (!err) {
+        console.error('Database is connected~!!\n');
     } else {
-        console.log('Error connecting Database~!!\n\n');
+        console.log('Error connecting Database~!!\n');
     }
 });
 
 app.get('/', function (req, res) {
     connection.query('select * from st_info', function (err, rows, fields) {
         connection.end();
-        if (err) {
+        if (!err) {
             res.send(rows);
             console.log("The solution is : ", rows);
         } else {
-            console.log('Error while performing Query.\n\n');
+            console.log('Error while performing Query.\n');
         }
     })
 })
 
 app.listen(8000, function () {
-    console.log("8000 Port : Server Started~!!\n\n")
+    console.log("8000 Port : Server Started~!!\n")
 })  
